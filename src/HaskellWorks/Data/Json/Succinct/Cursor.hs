a90255c9669c742a94e92c7c3a024526e600610e
John Ky <newhoggy@gmail.com>
2016-05-02 08:26:16 +1000

    Move jsonTokenAt function to submodule

--- src/HaskellWorks/Data/Json/Succinct/Cursor.hs
+++ src/HaskellWorks/Data/Json/Succinct/Cursor.hs
@@ -1,28 +1,8 @@
-{-# LANGUAGE FlexibleContexts      #-}
-{-# LANGUAGE FlexibleInstances     #-}
-{-# LANGUAGE InstanceSigs          #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
 
 module HaskellWorks.Data.Json.Succinct.Cursor
   ( module X
-  , jsonTokenAt
   ) where
 
-import qualified Data.Attoparsec.ByteString.Char8                           as ABC
-import           Data.ByteString.Internal                                   as BSI
-import           HaskellWorks.Data.Bits.BitWise
-import           HaskellWorks.Data.Json.Final.Tokenize.Internal
 import           HaskellWorks.Data.Json.Succinct.Cursor.CursorType          as X
 import           HaskellWorks.Data.Json.Succinct.Cursor.Internal            as X
-import           HaskellWorks.Data.Positioning
-import           HaskellWorks.Data.Succinct.RankSelect.Binary.Basic.Rank1
-import           HaskellWorks.Data.Succinct.RankSelect.Binary.Basic.Select1
-import           HaskellWorks.Data.Vector.VectorLike
-
-jsonTokenAt :: (Rank1 w, Select1 v, TestBit w) => JsonCursor ByteString v w -> Maybe JsonToken
-jsonTokenAt k = if balancedParens k .?. lastPositionOf (cursorRank k)
-  then case ABC.parse parseJsonToken (vDrop (toCount (jsonCursorPos k)) (cursorText k)) of
-    ABC.Fail    {}  -> error "Failed to parse token in cursor"
-    ABC.Partial _   -> error "Failed to parse token in cursor"
-    ABC.Done    _ r -> Just r
-  else Nothing
+import           HaskellWorks.Data.Json.Succinct.Cursor.Token               as X
