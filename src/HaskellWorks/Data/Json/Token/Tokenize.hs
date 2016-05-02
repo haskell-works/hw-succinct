eadc14cdc691923400c8e79f2a614ca428bab8f7
John Ky <newhoggy@gmail.com>
2016-05-02 23:22:37 +1000

    Move parser support to hw-parser project

--- src/HaskellWorks/Data/Json/Token/Tokenize.hs
+++ src/HaskellWorks/Data/Json/Token/Tokenize.hs
@@ -4,7 +4,6 @@
 module HaskellWorks.Data.Json.Token.Tokenize
     ( IsChar(..)
     , JsonToken(..)
-    , AFP.Parser(..)
     , parseJsonToken
     , parseJsonTokenString
     , escapedChar
@@ -12,38 +11,38 @@ module HaskellWorks.Data.Json.Token.Tokenize
 
 import           Control.Applicative
 import           Control.Monad
-import qualified Data.Attoparsec.ByteString.Char8          as BC
-import qualified Data.Attoparsec.Combinator                as AC
-import qualified Data.Attoparsec.Types                     as T
+import qualified Data.Attoparsec.ByteString.Char8   as BC
+import qualified Data.Attoparsec.Combinator         as AC
+import qualified Data.Attoparsec.Types              as T
 import           Data.Bits
 import           Data.Char
 import           Data.String
-import           HaskellWorks.Data.Attoparsec.Final.Parser as AFP
+import           HaskellWorks.Data.Parser           as P
 import           HaskellWorks.Data.Char.IsChar
 import           HaskellWorks.Data.Json.Token.Types
 
-hexDigitNumeric :: AFP.Parser t => T.Parser t Int
+hexDigitNumeric :: P.Parser t => T.Parser t Int
 hexDigitNumeric = do
   c <- satisfyChar (\c -> '0' <= c && c <= '9')
   return $ ord c - ord '0'
 
-hexDigitAlphaLower :: AFP.Parser t => T.Parser t Int
+hexDigitAlphaLower :: P.Parser t => T.Parser t Int
 hexDigitAlphaLower = do
   c <- satisfyChar (\c -> 'a' <= c && c <= 'z')
   return $ ord c - ord 'a' + 10
 
-hexDigitAlphaUpper :: AFP.Parser t => T.Parser t Int
+hexDigitAlphaUpper :: P.Parser t => T.Parser t Int
 hexDigitAlphaUpper = do
   c <- satisfyChar (\c -> 'A' <= c && c <= 'Z')
   return $ ord c - ord 'A' + 10
 
-hexDigit :: AFP.Parser t => T.Parser t Int
+hexDigit :: P.Parser t => T.Parser t Int
 hexDigit = hexDigitNumeric <|> hexDigitAlphaLower <|> hexDigitAlphaUpper
 
-verbatimChar :: AFP.Parser t => T.Parser t Char
+verbatimChar :: P.Parser t => T.Parser t Char
 verbatimChar  = satisfyChar (BC.notInClass "\"\\") <?> "invalid string character"
 
-escapedChar :: (IsString t, AFP.Parser t) => T.Parser t Char
+escapedChar :: (IsString t, P.Parser t) => T.Parser t Char
 escapedChar   = do
   _ <- string "\\"
   (   char '"'  >> return '"'  ) <|>
@@ -56,7 +55,7 @@ escapedChar   = do
     ( char '\'' >> return '\'' ) <|>
     ( char '/'  >> return '/'  )
 
-escapedCode :: (IsString t, AFP.Parser t) => T.Parser t Char
+escapedCode :: (IsString t, P.Parser t) => T.Parser t Char
 escapedCode   = do
   _ <- string "\\u"
   a <- hexDigit
@@ -65,49 +64,49 @@ escapedCode   = do
   d <- hexDigit
   return $ chr $ a `shift` 24 .|. b `shift` 16 .|. c `shift` 8 .|. d
 
-parseJsonTokenString :: (JsonTokenLike j, AFP.Parser t, Alternative (T.Parser t), IsString t) => T.Parser t j
+parseJsonTokenString :: (JsonTokenLike j, P.Parser t, Alternative (T.Parser t), IsString t) => T.Parser t j
 parseJsonTokenString = do
   _ <- string "\""
   value <- many (verbatimChar <|> escapedChar <|> escapedCode)
   _ <- string "\""
   return $ jsonTokenString value
 
-parseJsonTokenBraceL :: (JsonTokenLike j, AFP.Parser t, IsString t) => T.Parser t j
+parseJsonTokenBraceL :: (JsonTokenLike j, P.Parser t, IsString t) => T.Parser t j
 parseJsonTokenBraceL = string "{" >> return jsonTokenBraceL
 
-parseJsonTokenBraceR :: (JsonTokenLike j, AFP.Parser t, IsString t) => T.Parser t j
+parseJsonTokenBraceR :: (JsonTokenLike j, P.Parser t, IsString t) => T.Parser t j
 parseJsonTokenBraceR = string "}" >> return jsonTokenBraceR
 
-parseJsonTokenBracketL :: (JsonTokenLike j, AFP.Parser t, IsString t) => T.Parser t j
+parseJsonTokenBracketL :: (JsonTokenLike j, P.Parser t, IsString t) => T.Parser t j
 parseJsonTokenBracketL = string "[" >> return jsonTokenBracketL
 
-parseJsonTokenBracketR :: (JsonTokenLike j, AFP.Parser t, IsString t) => T.Parser t j
+parseJsonTokenBracketR :: (JsonTokenLike j, P.Parser t, IsString t) => T.Parser t j
 parseJsonTokenBracketR = string "]" >> return jsonTokenBracketR
 
-parseJsonTokenComma :: (JsonTokenLike j, AFP.Parser t, IsString t) => T.Parser t j
+parseJsonTokenComma :: (JsonTokenLike j, P.Parser t, IsString t) => T.Parser t j
 parseJsonTokenComma = string "," >> return jsonTokenComma
 
-parseJsonTokenColon :: (JsonTokenLike j, AFP.Parser t, IsString t) => T.Parser t j
+parseJsonTokenColon :: (JsonTokenLike j, P.Parser t, IsString t) => T.Parser t j
 parseJsonTokenColon = string ":" >> return jsonTokenColon
 
-parseJsonTokenWhitespace :: (JsonTokenLike j, AFP.Parser t, IsString t) => T.Parser t j
+parseJsonTokenWhitespace :: (JsonTokenLike j, P.Parser t, IsString t) => T.Parser t j
 parseJsonTokenWhitespace = do
   _ <- AC.many1' $ BC.choice [string " ", string "\t", string "\n", string "\r"]
   return jsonTokenWhitespace
 
-parseJsonTokenNull :: (JsonTokenLike j, AFP.Parser t, IsString t) => T.Parser t j
+parseJsonTokenNull :: (JsonTokenLike j, P.Parser t, IsString t) => T.Parser t j
 parseJsonTokenNull = string "null" >> return jsonTokenNull
 
-parseJsonTokenBoolean :: (JsonTokenLike j, AFP.Parser t, IsString t) => T.Parser t j
+parseJsonTokenBoolean :: (JsonTokenLike j, P.Parser t, IsString t) => T.Parser t j
 parseJsonTokenBoolean = true <|> false
   where
     true  = string "true"   >> return (jsonTokenBoolean True)
     false = string "false"  >> return (jsonTokenBoolean False)
 
-parseJsonTokenDouble :: (JsonTokenLike j, AFP.Parser t, IsString t) => T.Parser t j
+parseJsonTokenDouble :: (JsonTokenLike j, P.Parser t, IsString t) => T.Parser t j
 parseJsonTokenDouble = liftM jsonTokenNumber rational
 
-parseJsonToken :: (JsonTokenLike j, AFP.Parser t, IsString t) => T.Parser t j
+parseJsonToken :: (JsonTokenLike j, P.Parser t, IsString t) => T.Parser t j
 parseJsonToken =
   parseJsonTokenString     <|>
   parseJsonTokenBraceL     <|>
